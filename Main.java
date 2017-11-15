ba952f0b2735c771429800e7394a7aeaa70bec95
sunzhijuns <2843359242@qq.com>
2017-11-15 19:04:54 +0800

    基本概率
    在0.2的概率下，计算5次后，中奖的概率

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        int N = 100000000;
-        ThreeGatesExperiment exp = new ThreeGatesExperiment(N);
-        exp.run(true);
-        System.out.println();
-        exp.run(false);
+        int N = 1000000000;
+        double chance = 0.2;
+        int playTime = 5;
+        WinningPrize exp = new WinningPrize(chance, playTime, N);
+        exp.run();
+
     }
 }
