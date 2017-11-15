1e1e3c7432a309a33217bba65c3fbfbdff59fe1a
sunzhijuns <2843359242@qq.com>
2017-11-15 18:16:53 +0800

    Revert "用更大的数目估计PI"
    This reverts commit 2961db5109fb842a8bbdacc7e7bcd16ea1e42613.

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        int N = 100000000;
-        ThreeGatesExperiment exp = new ThreeGatesExperiment(N);
-        exp.run(true);
-        System.out.println();
-        exp.run(false);
-    }
-}
