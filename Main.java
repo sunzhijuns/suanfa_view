1558245dc470becc15f1667c2327affc9fa642bb
sunzhijuns <2843359242@qq.com>
2017-11-17 17:07:55 +0800

    读取迷宫数据文件

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int sceneWidth = 1200;
-        int sceneHeight = 800;
-        int N = 25;
-//        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth,sceneHeight,N);
-        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth,sceneHeight,N, HeapSortData.Type.Identical);
-        vis.start();
+//        int sceneWidth = 1200;
+//        int sceneHeight = 800;
+//        int N = 25;
+////        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth,sceneHeight,N);
+//        AlgoVisualizer vis = new AlgoVisualizer(sceneWidth,sceneHeight,N, HeapSortData.Type.Identical);
+//        vis.start();
+        String mazeFile = "maze_101_101.txt";
+        MazeData data = new MazeData(mazeFile);
+        data.print();
 
 
     }
