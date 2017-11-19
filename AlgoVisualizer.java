12cf43b79305354c74ad9c415fe6195ddf9384fc
sunzhijuns <2843359242@qq.com>
2017-11-19 23:22:38 +0800

    深度优先生成迷宫（非递归）

--- AlgoVisualizer.java
+++ AlgoVisualizer.java
@@ -5,8 +5,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Stack;
 
 public class AlgoVisualizer {
     private static final int[][] d = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
@@ -40,16 +39,26 @@ public class AlgoVisualizer {
         if (!data.inArea(x,y)){
             throw new IllegalArgumentException("out of area in go");
         }
-        data.visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
-            int newX = x + d[i][0] * 2;
-            int newY = y + d[i][1] * 2;
-            if (data.inArea(newX, newY) && !data.visited[newX][newY]){
-                setData(x + d[i][0],y + d[i][1]);
-                go(newX, newY);
+        Stack<Position> stack = new Stack<Position>();
+        stack.push(new Position(x,y,null));
+        data.visited[x][y] = true;
+        while (!stack.empty()){
+            Position curPos = stack.pop();
+            x = curPos.getX();
+            y = curPos.getY();
+            for (int i = 0; i < 4; i++) {
+                int newX = x + d[i][0] * 2;
+                int newY = y + d[i][1] * 2;
+                if (data.inArea(newX, newY) && !data.visited[newX][newY]){
+                    setData(x + d[i][0],y + d[i][1]);
+                    stack.push(new Position(newX, newY, curPos));
+                    data.visited[newX][newY] = true;
+                }
             }
         }
+
+
     }
 
     private void setData(int x, int y) {
